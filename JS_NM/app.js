fd10db657218d6970630a2efaa67525646763109
bomenakim <96710675+Bomena@users.noreply.github.com>
2022-06-11 01:42:22 +0900

    Learn about Array, and append method is "push"

--- JS_NM/app.js
+++ JS_NM/app.js
@@ -1,7 +1,13 @@
-const amIPretty = true;
-console.log(amIPretty); //true
+// Array
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
 
-const brain = null;
-let something;
-console.log(brain);     //null
-console.log(something); //value exist, but undefined
+// Get Item forom Array
+console.log(daysOfWeek[5]);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
+
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("kimbab");
+console.log(toBuy);
