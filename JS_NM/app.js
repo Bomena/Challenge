764d5a3e5c012c8e76f0b76e066faa8b7c165ea7
bomenakim <96710675+Bomena@users.noreply.github.com>
2022-06-12 06:45:48 +0900

    Learn about how to access HTML from JS

--- JS_NM/app.js
+++ JS_NM/app.js
@@ -1,14 +1,4 @@
-// condition and logic
-const age = parseInt(prompt("How old are you?"));
-
-if (isNaN(age) || age < 0) {
-    console.log("Please write a number.");
-} else if (age < 18) {
-    console.log("You are too young.");
-} else if (age <=50) {
-    console.log("You can drink.");
-} else if (age > 50 && age <= 80) {
-    console.log("You should exercise.");
-} else if (age > 80) {
-    console.log("You can do whatever you want.");
-}
+// Access HTML from JS
+const title = document.getElementById("title");
+console.dir(title);
+title.innerText = "Got you!";
