73c33c124ebd66e3d5105f994f7a0c6c4cd9ac4d
bomenakim <96710675+Bomena@users.noreply.github.com>
2022-06-11 12:40:44 +0900

    Learn about conditional and logic calculator

--- JS_NM/app.js
+++ JS_NM/app.js
@@ -1,32 +1,14 @@
-// Object
-const player = {
-    name: "spring",
-    points: 10,
-    fat: false,
+// condition and logic
+const age = parseInt(prompt("How old are you?"));
+
+if (isNaN(age) || age < 0) {
+    console.log("Please write a number.");
+} else if (age < 18) {
+    console.log("You are too young.");
+} else if (age <=50) {
+    console.log("You can drink.");
+} else if (age > 50 && age <= 80) {
+    console.log("You should exercise.");
+} else if (age > 80) {
+    console.log("You can do whatever you want.");
 }
-//console.log(player);
-//console.log(player.name);
-
-// Change name -> 내부에 있는 것을 바꿈. 객체 자체를 바꾸는 것이 아님
-player.name = "bom";
-//console.log(player.name);
-
-// Append lastName
-player.lastName = "potato";
-//console.log(player);
-
-function sayHello(nameOfPerson, age) {
-    console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
-}
-//sayHello("spring", 22);
-//sayHello("bom", 22);
-
-function plus(a, b) {
-    console.log(a + b);
-}
-//plus(3, 2);
-
-function divide(a, b) {
-    console.log(a / b);
-}
-//divide(4, 2);
