ee2e43ff31744786cfb418c836105bf6629515de
bomenakim <96710675+Bomena@users.noreply.github.com>
2022-06-12 12:07:21 +0900

    Learn about toggle

--- JS_NM/app.js
+++ JS_NM/app.js
@@ -1,14 +1,9 @@
 // add and remove class
+// toggle -> find classname
 const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-    const clickedClass = "clicked";
-    if (h1.classList.contains(clickedClass)) {
-        h1.classList.remove(clickedClass);
-    } else {
-        h1.classList.add(clickedClass);
-    }
+    h1.classList.toggle("clicked");
 }
-
 
 h1.addEventListener("click", handleTitleClick);
