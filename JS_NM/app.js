ab82263d585184ab86cfdeaa852b534456a19986
bomenakim <96710675+Bomena@users.noreply.github.com>
2022-06-12 09:36:42 +0900

    Learn about many event.

--- JS_NM/app.js
+++ JS_NM/app.js
@@ -1,11 +1,40 @@
 // Access HTML from JS
 // When start event, run function.
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-    console.log("title was clicked!");
-    title.style.color = "blue";
+    h1.style.color = "blue";
 }
 
+function handleMouseEnter() {
+    h1.innerText = "Mouse is here!";
+}
 
-title.addEventListener("click", handleTitleClick);
+function handleMouseLeave() {
+    h1.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+    alert("copier!!!");
+}
+
+function handleWindowOffline() {
+    alert("SOS no WIFI!!");
+}
+function handleWindowOnline() {
+    alert("ALL GOOD~");
+}
+h1.addEventListener("click", handleTitleClick);
+// = h1.onclick = handleTitleClick;
+h1.addEventListener("mouseenter", handleMouseEnter);
+// = h1.onmouseenter = handleTitleMouseEnter;
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
