e1bc854a685b5ba49529fd0804c6ca04690cfc5f
bomenakim <96710675+Bomena@users.noreply.github.com>
2022-06-13 19:10:48 +0900

    Add files via upload

--- JS_NM/app.js
+++ JS_NM/app.js
@@ -1,20 +1,31 @@
 // get information from input(without refresh)
-// save username in local storage
+// save username in local storage 
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
-
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(info) {
     info.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("username", username);
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME)
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGrettings();
 }
 
+function paintGrettings() {
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGrettings();
+}
