0616bd7c6ce9e30284812b0f01a8b306846726a4
bomenakim <96710675+Bomena@users.noreply.github.com>
2022-06-13 18:32:56 +0900

    Learn about how to save username in local storage

--- JS_NM/app.js
+++ JS_NM/app.js
@@ -1,11 +1,20 @@
 // get information from input(without refresh)
+// save username in local storage
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 
 function onLoginSubmit(info) {
     info.preventDefault();
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem("username", username);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME)
 }
+
 
 loginForm.addEventListener("submit", onLoginSubmit);
